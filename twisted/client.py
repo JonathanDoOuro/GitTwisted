b39e2d19b79e815f4bb59dc3442dcc9e686f2f49
Jonathan do Ouro <j248364@dac.unicamp.br>
2022-08-17 20:24:02 -0300

    chat

--- twisted/client.py
+++ twisted/client.py
@@ -1,18 +1,32 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
-from twisted.internet.protocol import ClientFactory as CliFactory
+from twisted.internet.protocol import ReconnectingClientFactory as CliFactory
 from twisted.internet.endpoints import TCP4ClientEndpoint
+from sys import stdout
 
 class Client(Protocol):
+    def __init(self):
+        reactor.callInThread()
+
     def dataReceived(self, data):
         data = data.decode('utf-8')
-        print(data)
-        self.transport.write(input(":::").encode('utf-8'))
+        stdout.write(data)
+
+    def send_data(self):
+        while True:
+            self.transport.write(input().encode('utf-8'))
 
 class ClientFactory(CliFactory):
     def buildprotocol(self, addr):
         return Client()
 
+    def clientConnectionFailed(self, connector, reason):
+        print(reason)
+        CliFactory.clientConnectionFailed(self, connector, reason)
+
+    def clientConnectionLost(self, connector, reason):
+        print(reason)
+        CliFactory.clientConnectionLost(self, connector, reason)
 
 if __name__ == '__main__':
     endpoint = TCP4ClientEndpoint(reactor, 'localhost', 2000)
--- twisted/server.py
+++ twisted/server.py
@@ -1,23 +1,43 @@
+from unicodedata import name
 from twisted.internet import reactor
-from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Protocol, connectionDone
 from twisted.internet.protocol import ServerFactory as servFactory
 from twisted.internet.endpoints import TCP4ServerEndpoint
+
 class Server(Protocol):
     def __init__(self, users):
         self.users = users
+        self.name = ""
 
     def connectionMade(self):
         print('new conncetion')
-        self.users.append(self)
         self.transport.write("Hello from server\n".encode('utf-8'))
+        self.transport.write("Write Your name: ".encode('utf-8'))
+    
+    def addUser(self, nameRecived):         
+        if nameRecived not in self.users:
+            self.users[self] = nameRecived
+            self.name = nameRecived
+        else:
+            self.transport.write("Wrong username, try another one".encode("utf-8"))
     
     def dataReceived(self, data: bytes):
-        for user in self.users:
-            user.transport.write(data)
+        data = data.decode("utf-8")
+
+        if not self.name:    
+            self.addUser(data)
+            return
+
+        for protocol in self.users.keys():
+            if(protocol != self):
+                protocol.transport.write(f"{self.name}: {data}".encode("utf-8") )
+    
+    def connectionLost(self, reason = connectionDone):
+        del self.users[self]
 
 class ServerFactory(servFactory):
     def __init__(self):
-        self.users = []
+        self.users = {}
 
     def buildProtocol(self, addr):
         return Server(self.users)
